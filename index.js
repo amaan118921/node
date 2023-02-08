f2e85459d397f8fcec4659ee246efceaa0365c00
amaan <amaanurrahman00@gmail.com>
2023-02-08 11:47:26 +0530

    final commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 require('dotenv/config')
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 const products = require('./routes/products')
 mongoose.connect(process.env.MONGO_STRING, {useNewUrlParser: true}, {strictQuery: true})
 
